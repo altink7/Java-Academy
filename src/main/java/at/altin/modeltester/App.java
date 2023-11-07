f337896a56bac68020bc918ab4486417d3bb9eaf
altink7 <kelmendialtin48@gmail.com>
2023-11-07 12:30:35 +0100

    Initial commit

---
+++ src/main/java/at/altin/modeltester/App.java
@@ -0,0 +1,13 @@
+package at.altin.modeltester;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
