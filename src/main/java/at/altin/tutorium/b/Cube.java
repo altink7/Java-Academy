39cd823a6dd36394501c0fc7e84fdc4267300f4e
altink7 <kelmendialtin48@gmail.com>
2023-11-09 10:15:11 +0100

    b: cube impl. and refactor

--- src/main/java/at/altin/tutorium/b/Cube.java
+++ src/main/java/at/altin/tutorium/b/Cube.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class Cube {
 
     //Alternative
+    @SuppressWarnings("unused")
     static Scanner SCANNER = new Scanner(System.in);
 
     public static void main(String[] args) {
@@ -20,10 +21,24 @@ public class Cube {
         double surfaceAreaAlt = 6 * (Math.pow(lengthOfASide, 2));
         double volumeAlt = Math.pow(lengthOfASide,3);
 
+        System.out.println("##############################");
+
+        System.out.printf("\nLength of a side: %d", lengthOfASide);
+        System.out.printf("\nSurface Area: %.2f", surfaceArea);
+        System.out.printf("\nVolume: %.2f\n", volume);
+
+        System.out.println("##############################");
+
 
         System.out.printf("\nLength of a side: %d", lengthOfASide);
         System.out.printf("\nSurface Area: %.2f", surfaceAreaAlt);
-        System.out.printf("\nVolume: %.2f", volumeAlt);
+        System.out.printf("\nVolume: %.2f\n", volumeAlt);
+
+        System.out.println("##############################");
+
+        System.out.println("Good Bye!");
+
+
 
         sc.close();
     }
