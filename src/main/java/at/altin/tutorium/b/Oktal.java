6dadaf5934771ac6fb83776f8098825c3e7f0e0f
altink7 <kelmendialtin48@gmail.com>
2023-11-09 11:33:26 +0100

    b: reformat

--- src/main/java/at/altin/tutorium/b/Oktal.java
+++ src/main/java/at/altin/tutorium/b/Oktal.java
@@ -6,18 +6,19 @@ public class Oktal {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("input: ");
+        System.out.print("input (oktalzahl): ");
         long input = sc.nextLong();
 
         long dezimal = 0;
         int i = 0;
 
         while(input != 0) {
+            // Formel aus dem Beispiel, siehe PDF
             dezimal += (long) ((input % 10) * Math.pow(8, i));
             input /= 10;
             i++;
         }
-        System.out.printf("\nDezimal: %d", dezimal);
+        System.out.printf("\nOutput(dezimal): %d", dezimal);
         sc.close();
     }
 }
