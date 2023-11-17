1e1e105660e0e7525804e63985909865caa7b3f0
altink7 <kelmendialtin48@gmail.com>
2023-11-17 11:32:35 +0100

    c: diagonale impl.

---
+++ src/main/java/at/altin/tutorium/c/Diagonale.java
@@ -0,0 +1,39 @@
+package at.altin.tutorium.c;
+
+import java.util.Scanner;
+
+public class Diagonale {
+
+    public static final char DIAGONAL_SYMBOL = '*';
+    public static final char STANDARD_SYMBOL = '-';
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(": ");
+        int seitenLaenge = scanner.nextInt();
+
+
+        while (seitenLaenge % 2 == 0 || seitenLaenge <= 1) {
+            System.out.println("bitte eine gueltige zahl eingeben!");
+            System.out.print(": ");
+            seitenLaenge = scanner.nextInt();
+        }
+
+        int linkeSeite = seitenLaenge; // sterne von links
+        int rechteSeite = seitenLaenge; // sterne von rechts
+
+        for(int i = seitenLaenge; i > 0; i--) {
+            for(int j = seitenLaenge; j>0; j--) {
+                char symbol =  (linkeSeite-j==0) || rechteSeite -linkeSeite == j-1 ? DIAGONAL_SYMBOL : STANDARD_SYMBOL;
+                System.out.print(symbol);
+            }
+
+            System.out.println("\n");
+            linkeSeite--;
+        }
+
+        scanner.close();
+    }
+
+
+}
