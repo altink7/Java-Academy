a2677a8c911ba46c951ead9edd22c6668505c31d
altink7 <kelmendialtin48@gmail.com>
2024-01-22 14:03:33 +0100

    DM Tut von 22.01.2024

---
+++ src/data/practice/Uebung_9_1_x.sql
@@ -0,0 +1,31 @@
+/*-----------------------------------------------
+Übung 9.1.1
+Geben Sie nur jene Personen (Vorname, Nachname) aus, deren Gehalt über dem Durchschnitt liegt.
+Sortieren Sie absteigend nach Gehalt und aufsteigend nach Nachnamen und Vornamen.
+-----------------------------------------------*/
+
+SELECT VORNAME, NACHNAME FROM PERSON p
+JOIN PERSONAL P2 on p.PERSONID = P2.PERSONID
+JOIN GEHALTSSTUFE g on P2.GEHALTSSTUFEID = g.GEHALTSSTUFEID
+WHERE MONATSGEHALT >
+      (SELECT AVG(MONATSGEHALT) FROM PERSONAL JOIN GEHALTSSTUFE
+          ON PERSONAL.GEHALTSSTUFEID = GEHALTSSTUFE.GEHALTSSTUFEID)
+ORDER BY VORNAME, NACHNAME;
+
+
+
+/*-----------------------------------------------
+Übung 9.1.2
+Geben Sie eine Liste aller Personen aus (Vorname, Nachname), für die keine Nationalität angegeben ist.
+Sortieren Sie aufsteigend nach Nachnamen und Vornamen. (ohne Join)
+-----------------------------------------------*/
+SELECT VORNAME, NACHNAME FROM PERSON p
+WHERE p.PERSONID NOT IN (SELECT PERSONID FROM REISEPASS)
+ORDER BY NACHNAME, VORNAME;
+
+
+-- UION ALL  Beispiel
+SELECT VORNAME FROM PERSON p
+WHERE p.PERSONID = 1 OR 1 = 1 UNION ALL (SELECT NACHNAME FROM PERSON);
+
+
